42422fdf3a9cead20bac6ecb8194c84c39864933
Michael Lanthier <m2lanthier@gmail.com>
2022-08-23 07:29:20 -0700

    Deduplicator can now send to another directory where only unique files will be stored. Currently copy to a new directory is supported.

--- cmd/dedupe-agent/main.go
+++ cmd/dedupe-agent/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"os"
+	"path/filepath"
 	"photo-deduplicator/internal/deduplicator"
+	"strconv"
 	"sync"
 
+	"github.com/google/uuid"
 	"github.com/pborman/getopt/v2"
 	"github.com/sirupsen/logrus"
 )
@@ -15,22 +20,24 @@ var log = logrus.New()
 func main() {
 
 	// Default values
-	// TODO: Maybe move these to parameter store and then this can all be stored in a config
-	// Maybe an extract parameters function that either uses parameter store during ci/cd
 	var (
 		help                bool
 		verbose             bool
 		hashingRoutineCount = 4
-		directory           = "photos/"
+		inputDirectory      = "photos/"
+		outputDirectory     = ""
 		logFileName         = ""
+		purge               = false
 	)
 
 	// Take in arguments
 	getopt.FlagLong(&help, "help", 'h', "Help")
 	getopt.FlagLong(&verbose, "verbose", 'v', "Verbose printing")
 	getopt.FlagLong(&hashingRoutineCount, "hashingRoutineCount", 'c', "Number of routines hashing the files.")
-	getopt.FlagLong(&directory, "directory", 'd', "Directory to deduplicate.")
+	getopt.FlagLong(&inputDirectory, "input", 'i', "Directory to deduplicate.")
+	getopt.FlagLong(&outputDirectory, "output", 'o', "Directory to store deduplicated files")
 	getopt.FlagLong(&logFileName, "logFile", 'L', "Log file")
+	getopt.FlagLong(&purge, "purge", 'p', "Purge deduplicated files")
 
 	// Parse arguments
 	getopt.Parse()
@@ -64,10 +71,55 @@ func main() {
 	// List out the arguments
 	log.Info("**Application Configuration**")
 	log.Info("Hashing Routines: ", hashingRoutineCount)
-	log.Info("Directory: ", directory)
+	log.Info("Input Directory: ", inputDirectory)
+	log.Info("Output Directory: ", outputDirectory)
+	log.Info("Purge: ", strconv.FormatBool(purge))
 	log.Info("Log file: ", logFileName)
 
-	deduper := deduplicator.New(directory, hashingRoutineCount)
+	// Data validation
+
+	// Verify info
+	inputDirectoryInfo, err := os.Stat(inputDirectory)
+	if err != nil {
+		// Error trying to read directory
+		log.Errorf("Error reading the input directory %s\n", inputDirectory)
+		log.Errorf("%s\n", err.Error())
+		fmt.Printf("Error reading from input directory %s\n", inputDirectory)
+		return
+	}
+	if !inputDirectoryInfo.IsDir() {
+		// Not valid directory
+		log.Errorf("Input directory (%s) is not a directory\n", inputDirectory)
+		fmt.Printf("Looks like %s is not a directory. Exiting\n", inputDirectory)
+		return
+	}
+
+	if outputDirectory != "" {
+		// validate output directory if it exists
+		outputDirectoryInfo, err := os.Stat(outputDirectory)
+		if errors.Is(err, os.ErrNotExist) {
+			// Create a new directory
+			err := os.Mkdir(outputDirectory, 0750)
+			if err != nil && !os.IsExist(err) {
+				log.Errorf("%s\n", err.Error())
+				fmt.Printf("Unable to create the output directory %s", outputDirectory)
+			}
+		} else if err != nil {
+			// Error trying to read directory
+			log.Errorf("Error reading the output directory %s\n", outputDirectory)
+			log.Errorf("%s\n", err.Error())
+			fmt.Printf("Error reading from output directory %s\n", outputDirectory)
+			return
+		} else if !outputDirectoryInfo.IsDir() {
+			// Not a directory
+			log.Fatalf("Output directory (%s) is not a directory\n", outputDirectory)
+			fmt.Printf("Looks like output directory %s is not a directory. Exiting\n", outputDirectory)
+			return
+		}
+	}
+
+	// Start deduplication
+	deduper := deduplicator.New(inputDirectory, hashingRoutineCount)
 	deduper.SetBufferSize(50)
 	photoChannel := make(chan deduplicator.DedupeFileMetadata, 100)
 	var photoWaitGroup sync.WaitGroup
@@ -76,14 +128,77 @@ func main() {
 
 	totalDuplicates := 0
 
+	failedCopies := []deduplicator.DedupeFileMetadata{}
+
+	// Process photo channel
 	for photoMetadata := range photoChannel {
 
 		if photoMetadata.DuplicatePath != "" {
 			totalDuplicates += 1
 			fmt.Printf("%s is a duplicate of %s\n", photoMetadata.Path, photoMetadata.DuplicatePath)
+			continue
+		}
+
+		if outputDirectory == "" {
+			continue
+		}
+
+		// Copy the file to the new directory
+		sourceFile, err := os.OpenFile(photoMetadata.Path, os.O_RDWR, 0666)
+
+		if err != nil {
+			// Error opening the file
+			failedCopies = append(failedCopies, photoMetadata)
+			log.Errorf("Error opening source photo (%s) (%s)\n", photoMetadata.Path, err.Error())
+			continue
+		}
+
+		uuid, err := uuid.NewRandom()
+
+		if err != nil {
+			// Error generating UUID
+			log.Errorf("Error generating uuid for photo name (%s)\n", err.Error())
+			failedCopies = append(failedCopies, photoMetadata)
+			continue
+		}
+
+		destinationFileName := filepath.Join(outputDirectory, (uuid.String() + ".jpg"))
+
+		destinationFile, err := os.OpenFile(destinationFileName, os.O_RDWR|os.O_CREATE, 0666)
+		if err != nil {
+			log.Errorf("Unable to open destination file %s(%s)\n", destinationFileName, err.Error())
+			failedCopies = append(failedCopies, photoMetadata)
+			continue
+		}
+
+		// Copy the photo
+		if _, err := io.Copy(destinationFile, sourceFile); err != nil {
+			log.Errorf("Unable to copy %s to %s (%s)\n", photoMetadata.Path, destinationFile, err.Error())
+			failedCopies = append(failedCopies, photoMetadata)
+			continue
+		}
+
+		// Flush to disk
+		if err := destinationFile.Sync(); err != nil {
+			log.Errorf("Unable to flush %s to disk (%s)\n", destinationFileName, err.Error())
+			failedCopies = append(failedCopies, photoMetadata)
+			continue
+		}
+
+		// Close file
+
+		if err := destinationFile.Close(); err != nil {
+			log.Errorf("Unable to close %s (%s)\n", destinationFileName, err.Error())
+			continue
+		}
+
+		if purge {
+			// delete the old file
 		}
 
 	}
+
+	fmt.Println("Deduplicated", totalDuplicates, "photos")
 
 	photoWaitGroup.Wait()
 
